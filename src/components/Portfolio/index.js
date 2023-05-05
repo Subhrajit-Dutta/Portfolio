5bc69238a1671dbc7e72ce79ecb26fa0631067ae
Subhrajit <subhrajitdutta007@gmail.com>
2023-05-05 14:24:22 +0530

    Projects added to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
+import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => { 
     const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -19,14 +17,6 @@ const Portfolio = () => {
         }
     });
 
-    useEffect(() => {
-        getPortfolio();
-    }, []);
-
-    const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio'));
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
-    }
 
     const renderPortfolio = (portfolio) => {
         return (
@@ -36,11 +26,11 @@ const Portfolio = () => {
                         return (
                             <div className="image-box" key={idx}>
                                 <img 
-                                src={port.image}
+                                src={port.cover}
                                 className="portfolio-image"
                                 alt="portfolio" />
                                 <div className="content">
-                                    <p className="title">{port.name}</p>
+                                    <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
                                     <button
                                         className="btn"
@@ -66,7 +56,7 @@ const Portfolio = () => {
                         idx={15}
                     />
                 </h1>
-                <div>{renderPortfolio(portfolio)}</div>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
             <Loader type="pacman" />
         </>
